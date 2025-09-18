2c324c0ef8876480653a9dcdf4679058a68bd7df
Nayan Kunwar <nayankunwar678@gmail.com>
2025-09-19 02:02:26 +0530

    migration sync

--- prisma/migrations/20250918202203_sync_db/migration.sql
+++ prisma/migrations/20250918202203_sync_db/migration.sql
@@ -3,7 +3,9 @@ CREATE TABLE "public"."User" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "passwordHash" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
